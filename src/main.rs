2f3798eef98e1dfa1d939b6198fdd1db525cfbcb
Kedap <kedap.dev@protonmail.com>
2024-12-29 00:11:16 -0600

    feat(main): agregando pwm

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::simple_pwm::{IntoPwmPin, Prescaler, Timer1Pwm};
 use panic_halt as _;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let timer1 = Timer1Pwm::new(dp.TC1, Prescaler::Prescale64);
 
-    let mut verde = pins.d3.into_output();
-    let mut amarillo = pins.d4.into_output();
-    let mut rojo = pins.d5.into_output();
+    let mut rojo = pins.d9.into_output().into_pwm(&timer1);
+    rojo.enable();
 
     loop {
-        verde.toggle();
-        arduino_hal::delay_ms(1000);
-        verde.toggle();
-        amarillo.toggle();
-        arduino_hal::delay_ms(1000);
-        amarillo.toggle();
-        rojo.toggle();
-        arduino_hal::delay_ms(1000);
-        rojo.toggle();
+        for x in (0..255).chain((0..255).rev()) {
+            rojo.set_duty(x);
+            arduino_hal::delay_ms(1000);
+        }
     }
 }
