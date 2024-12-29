ca03f001c509c379f9c2bc35be400bdd67243756
Kedap <kedap.dev@protonmail.com>
2024-12-29 14:47:51 -0600

    feat(main): corrigiendo el timer

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,13 @@ fn main() -> ! {
     let pins = arduino_hal::pins!(dp);
     let timer1 = Timer1Pwm::new(dp.TC1, Prescaler::Prescale64);
 
-    let mut rojo = pins.d9.into_output().into_pwm(&timer1);
+    let mut rojo = pins.d10.into_output().into_pwm(&timer1);
     rojo.enable();
 
     loop {
         for x in (0..255).chain((0..255).rev()) {
             rojo.set_duty(x);
-            arduino_hal::delay_ms(1000);
+            arduino_hal::delay_ms(10);
         }
     }
 }
