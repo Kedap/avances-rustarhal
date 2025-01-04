6d316ddb9f99f0d75d1a4eace8b4db4d881d3ba1
Kedap <kedap.dev@protonmail.com>
2025-01-04 13:48:07 -0600

    feat(main): leyendo los sensores

--- src/main.rs
+++ src/main.rs
@@ -22,18 +22,36 @@ fn main() -> ! {
     let mut s2 = pins.a2.into_output();
     let mut s3 = pins.a3.into_output();
     led.set_high();
+    let mut sensores = [0_u16; 16];
     println!("Setup completado {:#?}", 15.to_bin());
 
     loop {
-        s0.set_low();
-        s1.set_low();
-        s2.set_high();
-        s3.set_low();
+        (0..16).for_each(|i| {
+            let digital = (i as u8).to_bin();
+            if digital[0] == 0 {
+                s0.set_low();
+            } else {
+                s0.set_high();
+            }
+            if digital[1] == 0 {
+                s1.set_low();
+            } else {
+                s1.set_high();
+            }
+            if digital[2] == 0 {
+                s2.set_low();
+            } else {
+                s2.set_high();
+            }
+            if digital[3] == 0 {
+                s3.set_low();
+            } else {
+                s3.set_high();
+            }
+            sensores[i] = adc.read_blocking(&arduino_hal::adc::channel::ADC6);
+        });
 
-        println!(
-            "Lectura: {}",
-            adc.read_blocking(&arduino_hal::adc::channel::ADC6)
-        );
+        println!("Lectura: {:?}", sensores);
         arduino_hal::delay_ms(100);
     }
 }
